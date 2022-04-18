9e808673636ed1995e5e1d9323110010e349df5d
shammi77 <nusratshammi.vutu@gmail.com>
2022-04-19 01:53:04 +0600

    sevice part complete

--- src/components/Home/Service/Service.js
+++ src/components/Home/Service/Service.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
 
-const Service = ({service}) => {
-    const {name,img,price,description} = service;
-    return (
-        <div>
-            <img src={img}  alt=''/>
-            <h2>Service: {name}</h2>
-            <h3>Price: {price}</h3>
-            <h4>Description: {description}</h4>
-            <Button>Book Now: {name}</Button>
-        </div>
-    );
+const Service = ({ service }) => {
+  const { name, img, price, description } = service;
+  return (
+    <div className="g-5 col-sm-12 col-md-6 col-lg-4">  
+      <div className="card">
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>Package Name: {name}</Card.Title>
+        <Card.Text>Price : {price}</Card.Text>
+        <Card.Text> Package Details : {description}</Card.Text>
+        
+        <Button variant="primary">Book Now : {name}</Button>
+      </Card.Body>
+      </div>
+  
+    </div>
+  );
 };
 
 export default Service;
